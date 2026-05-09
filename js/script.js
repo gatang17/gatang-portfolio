45abdce306dbfde39b41b43ed36f83c7ecf8d403
Gretel Alvarez <gretelalvareztang@gmail.com>
2026-05-09 13:07:26 -0400

    final revision

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// =============================
-// GOOGLE ANALYTICS
-// =============================
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-BYEHMP81Y0');
-
 
 // =============================
 //BREADCUMBS
